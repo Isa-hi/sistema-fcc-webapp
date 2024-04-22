f364e974e0a2e9ec0c642d7b7004e4777c2adf3c
Isa-hi <lacasademiamigochanel@gmail.com>
2024-04-21 19:30:33 -0600

    Connect HTML Registro Materias  Form to the Typescript
    The data of the form is handled correctly by the typescript, adding and
    removing days from the dias array, and then the data is passed to
    materias.service

--- src/app/screens/registro-materias-screen/registro-materias-screen.component.ts
+++ src/app/screens/registro-materias-screen/registro-materias-screen.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { MateriasService } from 'src/app/services/materias.service';
+
 
 @Component({
   selector: 'app-registro-materias-screen',
@@ -21,12 +23,20 @@ export class RegistroMateriasScreenComponent implements OnInit {
     {value: '2', nombre: 'Licenciatura en Cienicas de la Computación'},
     {value: '3', nombre: 'Ingenieria en Tecnologías de la Información'},
   ];
+
+  public errors: any = {};
+  public editar: boolean = false;
+  public materia: any = {};
   
     constructor(
       private location : Location,
+      private materiasService: MateriasService
+
     ) { }
   
     ngOnInit(): void {
+      // Inicialize materia 
+      this.materia = this.materiasService.esquemaMateria();
     }
 
     public regresar(){
@@ -35,5 +45,25 @@ export class RegistroMateriasScreenComponent implements OnInit {
 
     public registrar(){
       //Lógica para registrar una materia
+
+      //Validar los campos
+      this.errors = [];
+      // Sends the materia data to the service to validate
+      this.errors = this.materiasService.validarMateria(this.materia, this.editar);
+    }
+
+    public checkboxChange(event: any){
+      //Lógica para checkbox
+      // Event contains the checkbox day and the checked value (0 or 1, day)
+      if(event[0] == true){
+        // Add day to the array
+        this.materia.dias.push(event[1]);
+        //console.log("Dias: ", this.materia.dias);
+      } else {
+        // Remove day from the array
+        let index = this.materia.dias.indexOf(event[1]);
+        this.materia.dias.splice(index, 1);
+        //console.log("Dias: ", this.materia.dias);
+      }
     }
 }
---
+++ src/app/services/materias.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { ValidatorService } from './tools/validator.service';
+import { ErrorsService } from './tools/errors.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MateriasService {
+
+  constructor(
+    private validatorService: ValidatorService,
+    private errorService: ErrorsService,
+
+  ) { }
+
+  // Esquema para las materias sirve para atrapar los datos de las materias
+  public esquemaMateria(){
+    return {
+      'nrc': '',
+      'nombre_materia': '',
+      'seccion': '',
+      'dias': [],
+      'hora_inicio': '',
+      'hora_fin': '',
+      'salon': '',
+      'programa_educativo': '',
+    }
+  }
+
+  public validarMateria(data: any, editar: boolean){
+    console.log("Validando materia... ", data);
+    // Print day checked
+    // let error: any = [];
+
+
+    // if(!this.validatorService.required(data["nrc"])){
+    //   error["nrc"] = this.errorService.required;
+    // }
+
+    // if(!this.validatorService.required(data["nombre"])){
+    //   error["nombre"] = this.errorService.required;
+    // }
+
+    // if(!this.validatorService.required(data["seccion"])){
+    //   error["seccion"] = this.errorService.required;
+    // }
+
+    // if(!this.validatorService.required(data["dias"])){
+    //   error["dias"] = this.errorService.required;
+    // }
+
+    // if(!this.validatorService.required(data["hora_inicio"])){
+    //   error["hora_inicio"] = this.errorService.required;
+    // }
+
+    // if(!this.validatorService.required(data["hora_fin"])){
+    //   error["hora_fin"] = this.errorService.required;
+    // }
+
+    // if(!this.validatorService.required(data["salon"])){
+    //   error["salon"] = this.errorService.required;
+    // }
+
+    // if(!this.validatorService.required(data["programa_educativo"])){
+    //   error["programa_educativo"] = this.errorService.required;
+    // }
+
+    // return error;
+  }
+}
