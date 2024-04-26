723049a2a02053bb4f2dbe225f889e1ec61bc8b6
Isa-hi <lacasademiamigochanel@gmail.com>
2024-04-26 11:50:41 -0600

    Add dynamically load to charts
    Charts load dynamically the lables and data based on the information
    received from contar total usuarios. Requirements finished

--- src/app/screens/graficas-screen/graficas-screen.component.ts
+++ src/app/screens/graficas-screen/graficas-screen.component.ts
@@ -2,23 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import DatalabelsPlugin from 'chartjs-plugin-datalabels';
 import { AdministradoresService } from 'src/app/services/administradores.service';
 
+
 @Component({
   selector: 'app-graficas-screen',
   templateUrl: './graficas-screen.component.html',
   styleUrls: ['./graficas-screen.component.scss']
 })
 export class GraficasScreenComponent implements OnInit {
-  //Agregar chart-js-plugin-datalabels
   // Variables
   public total_users: any = {};
   // Histograma
   lineChartData = {
-    labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    labels: [''],
     datasets: [
       {
-        data : [89, 34, 43, 54, 28, 74, 93],
-        label: 'Registro de materias',
-        backgroundColor: '#F88406'
+        data : [0],
+        label: 'Registro de usuarios',
       }
     ]
   }
@@ -29,18 +28,18 @@ export class GraficasScreenComponent implements OnInit {
 
   // Bar Chart
   barChartData = {
-    labels: ['Desarrollo Web', 'Minería de datos', 'Redes', 'Móviles', 'Inteligencia Artificial'],
+    labels: [''],
     datasets: [
       {
-        data : [89, 34, 43, 54, 28],
-        label: 'Registro de materias',
+        data : [0],
+        label: 'Registro de usuarios',
         backgroundColor: [
           //Random colors
-          '#FF6633',
-          '#FFB399',
-          '#FF33FF',
-          '#FFFF99',
-          '#00B3E6'
+          '#9966ff',
+          '#4bc0c0',
+          '#ff9f40',
+          '#36a2eb',
+          '#ffcd56'
         ]
       }
     ]
@@ -52,16 +51,18 @@ export class GraficasScreenComponent implements OnInit {
 
   // Pie Chart
   pieChartData = {
-    labels: ['Administradores', 'Maestros', 'Alumnos'],
+    labels: [''],
     datasets: [
       {
-        data : [89, 34, 43],
+        data : [0],
         label: 'Registro de usuarios',
         backgroundColor: [
           //Random colors
-          '#FF6633',
-          '#FFB399',
-          '#FF33FF'
+          '#9966ff',
+          '#4bc0c0',
+          '#ff9f40',
+          '#36a2eb',
+          '#ffcd56'
         ]
       }
     ]
@@ -73,16 +74,18 @@ export class GraficasScreenComponent implements OnInit {
 
   // Doughnut Chart
   doughnutChartData = {
-    labels: ['Administradores', 'Maestros', 'Alumnos'],
+    labels: [''],
     datasets: [
       {
-        data : [89, 34, 43],
+        data : [0],
         label: 'Registro de usuarios',
         backgroundColor: [
           //Random colors
-          '#FF6633',
-          '#FFB399',
-          '#FF33FF'
+          '#9966ff',
+          '#4bc0c0',
+          '#ff9f40',
+          '#36a2eb',
+          '#ffcd56'
         ]
       }
     ]
@@ -107,10 +110,74 @@ export class GraficasScreenComponent implements OnInit {
       (response) => {
         this.total_users = response;
         console.log("Total users: ", this.total_users);
+        this.setLabelsToCharts(this.total_users);
+        this.setUsersDataToCharts(this.total_users);
       }, (error) => {
         alert("No se pudo obtener el total de cada rol de usuarios");
       }
     )
+  }
+
+  public setUsersDataToCharts(total_users: any){
+    console.log("Setting data to charts...", total_users, "This", this.total_users)
+    this.pieChartData = {
+        ...this.pieChartData,
+        datasets: [
+            {
+                ...this.pieChartData.datasets[0],
+                data: [total_users.Administradores, total_users.Maestros, total_users.Alumnos]
+            }
+        ]
+    };
+    this.barChartData = {
+        ...this.barChartData,
+        datasets: [
+            {
+                ...this.barChartData.datasets[0],
+                data: [total_users.Administradores, total_users.Maestros, total_users.Alumnos]
+            }
+        ]
+    };
+    this.doughnutChartData = {
+        ...this.doughnutChartData,
+        datasets: [
+            {
+                ...this.doughnutChartData.datasets[0],
+                data: [total_users.Administradores, total_users.Maestros, total_users.Alumnos]
+            }
+        ]
+    };
+    this.lineChartData = {
+        ...this.lineChartData,
+        datasets: [
+            {
+                ...this.lineChartData.datasets[0],
+                data: [total_users.Administradores, total_users.Maestros, total_users.Alumnos]
+            }
+        ]
+    };
+  }
+
+  public setLabelsToCharts(total_users: any){
+    //Get the keys from the total_users object
+    const labels = Object.keys(total_users); // labels is an array with the keys of the object total_users
+    console.log("Setting labels to charts...", labels);
+    this.barChartData = {
+        ...this.barChartData,
+        labels
+    };
+    this.doughnutChartData = {
+        ...this.doughnutChartData,
+        labels
+    };
+    this.pieChartData = {
+        ...this.pieChartData,
+        labels
+    };
+    this.lineChartData = {
+        ...this.lineChartData,
+        labels
+    };
   }
 
 }
--- src/app/services/materias.service.ts
+++ src/app/services/materias.service.ts
@@ -93,7 +93,6 @@ export class MateriasService {
   //Servicio para actualizar una materia
   public actualizarMateria(data: any): Observable <any>{
     console.log("Actualizando materia... ", data);
-    //var headers = new HttpHeaders({'Content-type': 'application/json'})
     return this.http.put<any>(`${environment.url_api}/materias-edit/`, data, httpOptions );
   }
 
